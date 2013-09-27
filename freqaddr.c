ee75723ef32012da86193f9620f05ed9c542d6da
Jromer94 <Jromer94@gmail.com>
2013-09-26 20:52:53 -0400

    files each memory address into a struct arr and counts instances of each

--- freqaddr.c
+++ freqaddr.c
@@ -2,12 +2,57 @@
 
 #include <stdio.h>
 
+typedef struct {
+
+	char memAdd[15];
+	int occurences;
+
+} memory; 
+
+
 int main() {
 
 	FILE *data; 
-	fp = fopen("test.dat", "r");
+	fp = fopen("test.dat", "rb");
 
 
+	int lines = 0; 
+
+	while(EOF != (scanf("%*[^\n]"), scanf("%*c")))
+		++lines;
+
+	
+	rewind(fp);
+
+	memory memArr[lines];
+	
+	int hold;
+	int memLoc = 0;	
+	
+	for(hold = 0; hold < lines; hold++) {
+		int exist = 0; 
+		char temp[15];
+		fgets(temp, 15, fp);
+		for(int i = 0; i < memLoc; i++){
+			
+			if( strcmp(temp, memArr[i].memAdd) == 0) {
+				
+				memArr[i].occurences++;
+				exist = 1;	
+				break;	
+			
+			}
+	
+		}		
+		
+		if(exist == 0) {
+
+			memArr[memLoc].memAdd = temp;
+			memArr[memLoc].occurences = 1;
+			memLoc++;
+		}
+	} 
+		
 
 }
 
