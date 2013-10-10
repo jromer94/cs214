4ecde91e22b752d942f7384f04f8fa543d95c5bc
Jromer94 <Jromer94@gmail.com>
2013-10-09 20:27:31 -0400

    finished need to debug

--- freqaddr.c
+++ freqaddr.c
@@ -1,58 +1,167 @@
-//freqaddr hw1 cs 211
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct {
+struct memory{
 
 	char memAdd[15];
 	int occurences;
+	int fileLoc;
+	struct memory *next;
+	
 
-} memory; 
+}; 
 
 
-int main() {
+int main(int argc, char **argv) {
+	printf("lolofdfdasfd/n");
 
 	FILE *data; 
-	fp = fopen("test.dat", "rb");
+	data = fopen(argv[2], "r");
+	printf("lolofdfdasfd/n");
+	int top;
+	top = atoi(argv[1]);
 
+	struct 	memory *root;
+	root = malloc(sizeof(struct memory));
+	struct memory *current;
+	char temp[15];	
+	int order = 0;
+	root->occurences = 0;
+	printf("lolofdfdasfd/n");
 
-	int lines = 0; 
+	while(NULL != fgets(temp, 15, data)) {
 
-	while(EOF != (scanf("%*[^\n]"), scanf("%*c")))
-		++lines;
-
-	
-	rewind(fp);
-
-	memory memArr[lines];
-	
-	int hold;
-	int memLoc = 0;	
-	
-	for(hold = 0; hold < lines; hold++) {
 		int exist = 0; 
-		char temp[15];
-		fgets(temp, 15, fp);
-		for(int i = 0; i < memLoc; i++){
+		if(root->occurences == 0){
+
+			order++;
+			root->next = 0;
+			strcpy(root->memAdd,temp);
+			root->occurences = 1;
+			root->fileLoc = order;
+			current = root;
+			continue;
+
+		}
+		current = root;
+		
+		while(current->next != 0){
 			
-			if( strcmp(temp, memArr[i].memAdd) == 0) {
+			if( strcmp(temp, current->memAdd) == 0) {
 				
-				memArr[i].occurences++;
+				current->occurences++;
 				exist = 1;	
 				break;	
 			
 			}
+			current = current->next;
 	
 		}		
 		
 		if(exist == 0) {
+			
+			struct memory *new = malloc(sizeof(struct memory));
+			order++;
+			strcpy(new->memAdd,temp);
+			new->occurences = 0;
+			new->next = 0;
+			new->fileLoc = order;
+			current->next = new;			
 
-			memArr[memLoc].memAdd = temp;
-			memArr[memLoc].occurences = 1;
-			memLoc++;
 		}
 	} 
+	
 		
+	if(top > order) {
 
+		printf("number of arguments exceeds total number of unique addresses /n");
+		return 1;
+	}
+	struct memory *prev;
+	struct memory *max;
+	struct memory *prevMax;
+
+	if(top != 0){		
+	int i;
+		for(i = 0; i < top; i++) { 
+
+			current = root;
+			prev = root;
+			max = root;
+			prevMax = root;		
+		
+			while(current->next != 0) {
+
+				if(current->occurences >= max->occurences && current->fileLoc < max->fileLoc){
+
+					prevMax=prev;
+					max=current;			
+
+				}
+			
+				prev=current;
+				current=current->next;
+
+			}
+
+			if(max == prevMax){
+
+				printf("stuff goes here");
+				root=root->next;	
+
+			} else {
+
+				printf("stuff goes here");
+				prevMax->next=max->next;
+
+			}
+
+
+		}
+
+	} else {
+
+		while(root->next != 0){
+
+			current = root;
+			prev = root;
+			max = root;
+			prevMax = root;		
+		
+			while(current->next != 0) {
+
+				if(current->occurences >= max->occurences && current->fileLoc < max->fileLoc){
+
+					prevMax=prev;
+					max=current;			
+
+				}
+			
+				prev=current;
+				current=current->next;
+
+			}
+
+			if(max == prevMax){
+
+				printf("stuff goes here");
+				root=root->next;	
+
+			} else {
+
+				printf("stuff goes here");
+				prevMax->next=max->next;
+
+			}
+
+
+		}
+
+		printf("stuff goes here");
+	}
+
+	return 0;
+	
 }
 
