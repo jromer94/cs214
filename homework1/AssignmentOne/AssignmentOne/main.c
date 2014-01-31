cea8b4af8c3ac12ad016f56d4307c2ce24cf3e88
Alex McCullough <alexmccullough@Alex-McCulloughs-MacBook-Pro.local>
2014-01-31 16:44:59 -0500

    added files

---
+++ homework1/AssignmentOne/AssignmentOne/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  AssignmentOne
+//
+//  Created by Alex McCullough on 1/31/14.
+//  Copyright (c) 2014 Alex McCullough. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
