40d9dab8d21fc655898065250b9610fb46baae50
Alex McCullough <alexmccullough@Alex-McCulloughs-MacBook-Pro.local>
2014-02-13 18:32:12 -0500

    homework2

--- homework2/Assignment2/Assignment2/main.c
+++ homework2/Assignment2/Assignment2/main.c
@@ -2,17 +2,34 @@
 //  main.c
 //  Assignment2
 //
-//  Created by Alex McCullough on 2/9/14.
+//  Created by Alex McCullough on 2/11/14.
 //  Copyright (c) 2014 Alex McCullough. All rights reserved.
 //
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[])
-{
 
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+//full credit for linear top operation
 
+//c++ iterator
+//for (iterator container.begin(), iter != container.end(), iter++){
+//  cont << iter->data;
+//}
+
+//accessing data through iterator is independent of advancing through iterator
+
+//java iterator
+//while (iter hasnext()){
+//  object data = iternext();
+//  System.out.println(data);
+//}
+
+
+//every node should contain a reference count of number of pointers pointing to the node at any given time (always atleast 1 when in the list)
+//if reference count is 0, delete item, free()
+
+//head of list, pointer to comparator function, pointer to destroyer function
+
+
+
+//library. use AR command and -l. libsl.a
--- homework2/AssignmentTwo/main.c
+++
@@ -1,9 +0,0 @@
-//
-//  main.c
-//  AssignmentTwo
-//
-//  Created by Alex McCullough on 2/9/14.
-//  Copyright (c) 2014 Alex McCullough. All rights reserved.
-//
-
-#include <stdio.h>
