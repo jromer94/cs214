bbf8423fcf23c3ddcb3f9d471ab18cb5e9d6d9ec
mccullal8 <mccullal8@gmail.com>
2014-02-17 17:47:37 -0500

    update from sakai

--- homework2/Assignment2/Assignment2/sorted-list.h
+++ homework2/Assignment2/Assignment2/sorted-list.h
@@ -11,7 +11,6 @@
  */
 struct SortedList
 {
-    
 };
 typedef struct SortedList* SortedListPtr;
 
@@ -22,7 +21,6 @@ typedef struct SortedList* SortedListPtr;
  */
 struct SortedListIterator
 {
-    
 };
 typedef struct SortedListIterator* SortedListIteratorPtr;
 
@@ -36,26 +34,27 @@ typedef struct SortedListIterator* SortedListIteratorPtr;
  * smaller, 0 if the two objects are equal, and 1 if the 2nd object is
  * smaller.
  *
- * Note that you are not expected to implement any comparator functions.
- * You will be given a comparator function when a new sorted list is
- * created.
+ * Note that you are not expected to implement any comparator or destruct
+ * functions.  You will be given a comparator function and a destruct
+ * function when a new sorted list is created.
  */
 
-typedef int (*CompareFuncT)(void *, void *);
-
+typedef int (*CompareFuncT)( void *, void * );
+typedef void (*DestructFuncT)( void * );
 
 /*
  * SLCreate creates a new, empty sorted list.  The caller must provide
  * a comparator function that can be used to order objects that will be
- * kept in the list.
- *
- * If the function succeeds, it returns a (non-NULL) SortedListT object.
- * Else, it returns NULL.
+ * kept in the list, and a destruct function that gets rid of the objects
+ * once they are no longer in the list or referred to in an iterator.
+ * 
+ * If the function succeeds, it returns a (non-NULL) SortedListT object,
+ * otherwise, it returns NULL.
  *
  * You need to fill in this function as part of your implementation.
  */
 
-SortedListPtr SLCreate(CompareFuncT cf, void* );
+SortedListPtr SLCreate(CompareFuncT cf, DestructFuncT df);
 
 /*
  * SLDestroy destroys a list, freeing all dynamically allocated memory.
@@ -71,7 +70,7 @@ void SLDestroy(SortedListPtr list);
  * of existing objects in the list, then the subset can be kept in any
  * order.
  *
- * If the function succeeds, it returns 1.  Else, it returns 0.
+ * If the function succeeds, it returns 1, othrewise it returns 0.
  *
  * You need to fill in this function as part of your implementation.
  */
@@ -81,23 +80,25 @@ int SLInsert(SortedListPtr list, void *newObj);
 
 /*
  * SLRemove removes a given object from a sorted list.  Sorted ordering
- * should be maintained.
+ * should be maintained.  SLRemove may not change the object whose
+ * pointer is passed as the second argument.  This allows you to pass
+ * a pointer to a temp object equal to the object in the sorted list you
+ * want to remove.
  *
- * If the function succeeds, it returns 1.  Else, it returns 0.
+ * If the function succeeds, it returns 1, otherwise it returns 0.
  *
  * You need to fill in this function as part of your implementation.
  */
 
 int SLRemove(SortedListPtr list, void *newObj);
-//find and remove single node, free()
-//
+
 
 /*
  * SLCreateIterator creates an iterator object that will allow the caller
  * to "walk" through the list from beginning to the end using SLNextItem.
  *
  * If the function succeeds, it returns a non-NULL pointer to a
- * SortedListIterT object.  Otherwise, it returns NULL.  The SortedListT
+ * SortedListIterT object, otherwise it returns NULL.  The SortedListT
  * object should point to the first item in the sorted list, if the sorted
  * list is not empty.  If the sorted list object is empty, then the iterator
  * should have a null pointer.
@@ -106,13 +107,12 @@ int SLRemove(SortedListPtr list, void *newObj);
  */
 
 SortedListIteratorPtr SLCreateIterator(SortedListPtr list);
-//when we create, points to first spot. Call SLnextItem which returns value of first pointer
-//once we call SLnextItem to return value, move iterator to next Item
+
 
 /*
  * SLDestroyIterator destroys an iterator object that was created using
  * SLCreateIterator().  Note that this function should destroy the
- * iterator but should NOT affectt the original list used to create
+ * iterator but should NOT affect the original list used to create
  * the iterator in any way.
  *
  * You need to fill in this function as part of your implementation.
